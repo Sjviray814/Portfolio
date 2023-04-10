27954fb50d2e4f8429a78ecc495c2d071a0b2bf1
sjviray814 <sjviray814@gmail.com>
2023-04-09 21:03:59 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+var i = 0;
+var txt = 'Stephen Viray.'; /* The text */
+var speed = 150; /* The speed/duration of the effect in milliseconds */
+
+function beginningAnimation() {
+  if (i < txt.length) {
+    document.getElementById("stephen").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(beginningAnimation, speed);
+  }
+  else{
+    document.getElementById("hello").classList.toggle('fadein');
+    document.getElementById("description").classList.toggle('fadein');
+
+  }
+}
+
+
+
+window.onload = function(){
+    beginningAnimation();
+    
+}
+
+
+
+
+
+// Code copy pasted from codepen for the hamburger menu:
+$(document).ready(function(){
+	$('#nav-icon3').click(function(){
+		$(this).toggleClass('open');
+    document.getElementById("links").classList.contains('hidden') ? document.getElementById("links").classList.replace('hidden', 'grid') : document.getElementById("links").classList.replace('grid', 'hidden');
+	});
+});
+
+
+
+// Code copied from fireship video, does animations when elements load onto screen: https://www.youtube.com/watch?v=T33NN_pPeNI
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) =>{
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+    // Uncomment this to make animations play forever
+    // else{
+    //   entry.target.classList.remove('show');
+    // }
+  })
+})
+
+const hiddenElements = document.querySelectorAll('.hide');
+hiddenElements.forEach((el) => observer.observe(el));
